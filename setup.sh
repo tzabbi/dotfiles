f43c7eb9c24e33887822f6ce47c99f1ed9cc7c6b
Tom Zaspel <tom@zaspel.it>
2024-10-26 19:48:17 +0200

    feat: rework tmux config
    Signed-off-by: Tom Zaspel <tom@zaspel.it>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 scriptdirectory=$(cd -- "$(dirname -- "$0")" && pwd)
 cd "$scriptdirectory"
+
 
 for dir in */; do
     stow "$(basename "$dir")"
