079a04a9e71a3fe5b2dd69f258a9330ebf9c308b
Tom Zaspel <tom@zaspel.it>
2024-10-27 11:02:28 +0100

    feat: add brew installation in setup script
    Signed-off-by: Tom Zaspel <tom@zaspel.it>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,36 @@
 #!/bin/sh
 
+
+if [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
+  echo "Installing and configuring brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo >> $HOME/.bashrc
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  if [ $(grep "^ID=" /etc/os-release | cut -d "=" -f 2) == "ubuntu" ]; then
+    sudo apt-get install build-essential
+    brew install gcc
+    brew bundle --file ./brew/Brewfile.work
+  fi
+  if [ $(grep "^ID=" /etc/os-release | cut -d "=" -f 2) == "fedora" ]; then
+    sudo dnf group install development-tools
+    brew install gcc
+    brew bundle --file ./brew/Brewfile.private
+  fi
+fi
+
 if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+  echo "Installing tmux plugin manager..."
   git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 scriptdirectory=$(cd -- "$(dirname -- "$0")" && pwd)
 cd "$scriptdirectory"
 
+echo "Removing .bashrc"
+rm $HOME/.bashrc
 
 for dir in */; do
-    stow "$(basename "$dir")"
+  echo "Creating link for  $dir ..."
+  stow "$(basename "$dir")"
 done
