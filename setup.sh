1381b637bccfebf71848f2c5a38377de3571c5ed
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2025-02-10 08:40:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-if [[ ! command -v git >/dev/null 2>&1 && ("$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "debian" || "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu") ]]; then
-  echo "installing git"
-  sudo apt update && sudo apt install -y git
-fi
-
 if [[ ! command -v curl >/dev/null 2>&1 && ("$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "debian" || "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu") ]]; then
   echo "installing curl"
   sudo apt update && sudo apt install -y curl
 fi
-
 
 # install homebrew if not installed
 if [[ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
