e9fab9541b21e940781b8b611d22cae75fcdad59
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2025-02-10 08:38:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-if [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
+if [[ ! command -v git >/dev/null 2>&1 && ("$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "debian" || "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu") ]]; then
+  
+
+
+# install homebrew if not installed
+if [[ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
   echo "Installing and configuring brew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo >> "$HOME/.bashrc"
   echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.bashrc"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   brew install gcc
-  if [ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu" ]; then
+  if [[ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu" ]]; then
     sudo apt-get install build-essential
   fi
-  if [ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "fedora" ]; then
+  if [[ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "fedora" ]]; then
     sudo dnf group install development-tools
   fi
 fi
 
-if [ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu" ]; then
+# install required brew packages based on used linux distro
+if [[ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu" ]]; then
   brew bundle --file ./brew/Brewfile.work
 fi
 
-if [ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "fedora" ]; then
+if [[ "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "fedora" ]]; then
   brew bundle --file ./brew/Brewfile.private
 fi
 
-if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+if [[ ! -d $HOME/.tmux/plugins/tpm ]]; then
   echo "Installing tmux plugin manager..."
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 fi
 
-if [ ! "$(grep -wq "path = ./.dotfiles_gitconfig" $HOME/.gitconfig)" ]; then
+if [[ ! "$(grep -wq "path = ./.dotfiles_gitconfig" $HOME/.gitconfig)" ]]; then
   printf "[include]\n    path = ./.dotfiles_gitconfig" >> "$HOME/.gitconfig"
 fi
 
