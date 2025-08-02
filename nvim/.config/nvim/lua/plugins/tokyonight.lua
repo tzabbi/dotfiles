20cfca10b4714055aeaf5513b9355e56c39a0f6c
Tom Zaspel <tom@zaspel.it>
2025-08-02 13:10:37 +0200

    feat: add even more plugins

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,12 @@ return {
   priority = 1000,
   opts = {},
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('tokyonight').setup {
+    styles = {
+        comments = { italic = false }, -- Disable italics in comments
+      },
+    }
     vim.cmd.colorscheme "tokyonight-moon"
   end
 }
