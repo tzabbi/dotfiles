975d021ce5ae23c1a4aff689097115c8a72f0ebf
Tom Zaspel <tom@zaspel.it>
2025-08-05 06:48:30 +0200

    feat: add docker-ls and bash-language-server to brew

--- setup.sh
+++ setup.sh
@@ -43,8 +43,10 @@ echo "Removing nvim config"
 rm "$HOME/.config/nvim"
 
 for dir in */; do
-  echo "Creating link for  $dir ..."
-  stow "$(basename "$dir")"
+  if [[ "$dir" != "lazyvim/" ]]; then
+    echo "Creating link for  $dir ..."
+    stow "$(basename "$dir")"
+  fi
 done
 
 if [[ "$(which curl)" == "/home/linuxbrew/.linuxbrew/bin/curl" && "$(which git)" == "/home/linuxbrew/.linuxbrew/bin/git" && ("$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "debian" || "$(grep "^ID=" /etc/os-release | cut -d "=" -f 2)" == "ubuntu") ]]; then
