05de849e5b11602d32d4190d897980e555a9d657
Tom Zaspel <tom@zaspel.it>
2025-08-05 07:05:25 +0200

    feat: add required parser for nvim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,11 @@ return {
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require 'nvim-treesitter.configs'.setup {
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "ruby", "terraform", "go", "gomod", "gosum",
+        "yaml", "toml", "dockerfile", "bash", "helm", "hcl", "gitconfig", "json", "sql", "xml"
+      },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
@@ -31,11 +33,11 @@ return {
         disable = { "c", "rust" },
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
         end,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
