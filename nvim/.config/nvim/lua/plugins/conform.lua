20f53297d9ca178241938095986ffc1a64f1da13
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2025-08-13 10:14:59 +0200

    Update conform.lua

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,13 +2,5 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    -- Definiere hier, welcher Formatter für welchen Dateityp verwendet wird.
-    -- Wir überschreiben hier den Standard für YAML.
-    formatters_by_ft = {
-      -- ... andere Dateitypen werden von der LazyVim-Standardkonfiguration übernommen
-      yaml = { "yamlfmt" },
-    },
-    -- Optional: Konfiguration für yamlfmt selbst, falls nötig.
-    -- In der Regel ist für dein Problem keine weitere Konfiguration nötig.
   },
 }
