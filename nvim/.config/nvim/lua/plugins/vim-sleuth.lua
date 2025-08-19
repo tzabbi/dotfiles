ffee9b0edeae08e059631ba4165cc71898d49b8b
Tom Zaspel <tom@zaspel.it>
2025-08-19 07:32:18 +0200

    feat: add vim sleuth plugin

---
+++ nvim/.config/nvim/lua/plugins/vim-sleuth.lua
@@ -0,0 +1,5 @@
+-- this is a vim plugin so we cannot configure it
+-- This plugin automatically adjusts 'shiftwidth' and 'expandtab' heuristically based on the current file
+return {
+  "tpope/vim-sleuth",
+}
