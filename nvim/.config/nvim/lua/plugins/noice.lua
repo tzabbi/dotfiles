bbe20489c89d54564f08e15750e9d5031789bec8
Tom Zaspel <tom@zaspel.it>
2025-08-24 18:51:54 +0200

    feat: configure noice seach bottom

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,31 +1,34 @@
 -- lazy.nvim
 -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    views = {
-      cmdline_popup = {
-        position = {
-          row = 10,
-          col = "50%",
-        },
-        size = {
-          width = 60,
-          height = "auto",
-        },
-      },
-    },
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-  },
-  config = function(_, opts)
-    require("noice").setup(opts)
-  end,
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		views = {
+			cmdline_popup = {
+				position = {
+					row = 5,
+					col = "50%",
+				},
+				size = {
+					width = 60,
+					height = "auto",
+				},
+			},
+		},
+		presets = {
+			bottom_search = true,
+		},
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+	config = function(_, opts)
+		require("noice").setup(opts)
+	end,
 }
