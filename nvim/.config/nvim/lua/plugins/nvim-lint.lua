66a47c07815a90a24be0c6e784e564946f74510b
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2025-09-01 08:52:04 +0200

    Update nvim-lint.lua

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,8 +1,25 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      markdown = { "markdownlint-cli2" },
+  {
+    "mfussenegger/nvim-lint",
+    -- no version available
+    event = {
+      "BufReadPre",
+      "BufNewFile",
     },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        -- ... andere Dateitypen ...
+        markdown = { "markdownlint" },
+      }
+
+      -- Linter automatisch bei jedem Speichern ausführen
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
   },
 }
