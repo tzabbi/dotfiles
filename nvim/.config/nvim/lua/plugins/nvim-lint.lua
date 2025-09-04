a3ee573f3c754fbd56e43efe5148396b6d8eb96f
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2025-09-04 08:04:17 +0200

    Update nvim-lint.lua

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,30 +1,25 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    -- no version available
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-    config = function()
-      local lint = require("lint")
+	{
+		"mfussenegger/nvim-lint",
+		-- no version available
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+			"InsertLeave",
+		},
+		config = function()
+			local lint = require("lint")
 
-      lint.linters_by_ft = {
-        markdown = { "markdownlint-cli2" },
-      }
+			lint.linters_by_ft = {
+				markdown = { "markdownlint" },
+			}
 
-      vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-        group = vim.api.nvim_create_augroup("linting", { clear = true }),
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-      
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = vim.api.nvim_create_augroup("Linter", { clear = true }),
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 }
