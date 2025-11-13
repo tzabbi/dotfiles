8d35b4add7dee94caf1fbc597609c46869f3ee62
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2025-11-13 07:18:19 +0100

    Update lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -21,9 +21,13 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+  defaults = { lazy = false },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { 
+      enabled = true,
+      notify = false
+    },
 })
