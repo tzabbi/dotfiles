eda2b9aa7a5a501a26ac35c37d0103f4e47e20db
Tom Zaspel <40226087+tzabbi@users.noreply.github.com>
2026-09-07 13:57:19 +0200

    Update keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -42,14 +42,14 @@ vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decreas
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase Window Width" })
 
 -- Move Lines
-vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<CR>==", { desc = "Move Line Down" })
-vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<CR>==", { desc = "Move Line Up" })
-vim.keymap.set("i", "<A-j>", "<Esc><cmd>m .+1<CR>==gi", { desc = "Move Line Down" })
-vim.keymap.set("i", "<A-k>", "<Esc><cmd>m .-2<CR>==gi", { desc = "Move Line Up" })
-vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<CR>gv=gv", { desc = "Move Selection Down" })
+vim.keymap.set("n", "<C-j>", "<cmd>execute 'move .+' . v:count1<CR>==", { desc = "Move Line Down" })
+vim.keymap.set("n", "<C-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<CR>==", { desc = "Move Line Up" })
+vim.keymap.set("i", "<C-j>", "<Esc><cmd>m .+1<CR>==gi", { desc = "Move Line Down" })
+vim.keymap.set("i", "<C-k>", "<Esc><cmd>m .-2<CR>==gi", { desc = "Move Line Up" })
+vim.keymap.set("v", "<C-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<CR>gv=gv", { desc = "Move Selection Down" })
 vim.keymap.set(
 	"v",
-	"<A-k>",
+	"<C-k>",
 	":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<CR>gv=gv",
 	{ desc = "Move Selection Up" }
 )
